1946a3e4527d46f6b50b18b4df787e23b4d3e841
Catherine <80923857+Zhmaeva@users.noreply.github.com>
2021-03-21 00:06:04 +0300

    Fix print

--- 1| Zhmaeva Ekaterina.playground/Contents.swift
+++ 1| Zhmaeva Ekaterina.playground/Contents.swift
@@ -10,6 +10,8 @@ import Darwin.C.math
 var a: Double = 5
 var b: Double = 18
 var c: Double = 0
+
+print(a, "x^2 +", b, "x +", c, "= 0")
 var x1: Double = 0
 var x2: Double = 0
 
@@ -21,13 +23,13 @@ if (D < 0) {
 
 if (D == 0) {
     x1 = (-b + sqrt(D)) / (2 * a)
-    print(x1)
+    print("x1 =", x1)
 }
 
 if (D > 0) {
     x1 = (-b + sqrt(D)) / (2 * a)
     x2 = (-b - sqrt(D)) / (2 * a)
-    print(x1, x2)
+    print("x1 =", x1, "\nx2 =", x2)
 }
 
 /*
@@ -41,6 +43,7 @@ var hipotenuse: Double = sqrt(pow(length, 2)+(pow(widht, 2)))
 var area: Double = ((length * widht) / 2)
 var perimeter: Double = (length + widht + hipotenuse)
 
+print("\n\n")
 print("Катет трегольника а = ", length)
 print("Катет треугольника b = ", widht)
 print("Гипотенуза треугольника = ", hipotenuse)
@@ -51,11 +54,11 @@ print("Периметр треугольника = ", perimeter)
  Задание 3|
  Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет.
  */
-
-print("Введите сумму вклада")
+print("\n\n")
 var amount: Double = 100000
-print("Введите процентную ставку")
+print("Сумма вклада", amount)
 var percent: Double = 8
+print("Процентная ставка", percent)
 let coefficient = percent / 100
 
 var year1 = amount + (amount * coefficient)
@@ -64,10 +67,9 @@ var year3 = year2 + (year2 * coefficient)
 var year4 = year3 + (year3 * coefficient)
 var year5 = year4 + (year4 * coefficient)
 
-print("изменение размера вклада по годам: \n",
+print("Изменение размера вклада по годам: \n",
 "1 год: ", year1, " рублей\n",
 "2 год: ", year2, " рублей\n",
 "3 год: ", year3, " рублей\n",
 "4 год: ", year4, " рублей\n",
 "5 год: ", year5, " рублей\n")
-  
