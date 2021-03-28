6af75defedfc20811aa6bdb0898be1b3339f8a2e
Catherine <scrumb@my.com>
2021-03-28 22:04:21 +0300

    lesson2- 1-5

--- 2|_Zhmaeva Ekaterina.playground/Contents.swift
+++ 2|_Zhmaeva Ekaterina.playground/Contents.swift
@@ -27,35 +27,65 @@ if (randomInt % 3) == 0 {
 print("------------------------------", "\n")
 
 //3. Создать возрастающий массив из 100 чисел.
-var arrayOf100 = [Int]()
-for i in 1...100 {
-    arrayOf100.append(i)
-}
-    print(arrayOf100)
+var indexArray = Array(1...100)
+    print("Массив из 100 ", indexArray)
 
 print("------------------------------", "\n")
 
 //4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
 
-
-for value in arrayOf100 {
+for value in indexArray {
     if (value % 2) == 0 {
-    arrayOf100.remove(at: arrayOf100.firstIndex(of: value)!)
+    indexArray.remove(at: indexArray.firstIndex(of: value)!)
     }
 }
-    print(arrayOf100)
+    print("Массив из нечетных чисел", indexArray)
 
 print("------------------------------", "\n")
 
-
-for value in arrayOf100 {
-    if (value % 3) == 0 {
-    arrayOf100.remove(at: arrayOf100.firstIndex(of: value)!)
+for value in indexArray {
+    if (value % 3) != 0 {
+    indexArray.remove(at: indexArray.firstIndex(of: value)!)
     }
 }
-    print(arrayOf100)
+    print("Массив из нечетных чисел делящихся на 3", indexArray)
 
 print("------------------------------", "\n")
-//5. Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 50 элементов (подробности на странице урока).
-//6. Заполнить массив из 100 элементов различными простыми числами (подробности на странице урока).
+
+//5. Написать функцию, которая добавляет в массив новое число Фибоначчи F0 = 0, F1 = 1, Fn = Fn=Fn-1 + Fn-2
+
+func fibonacci(_ n: Int) -> [Int] {
+    var arrayFibonacci = [Int]()
+    if n == 1 {
+        let f0 = 0
+        arrayFibonacci.append(f0)
+    } else if n == 2 {
+        let f1 = 1
+        let f0 = 0
+        arrayFibonacci.append(f0)
+        arrayFibonacci.append(f1)
+    } else {
+        let f1 = 1
+        let f0 = 0
+        arrayFibonacci.append(f0)
+        arrayFibonacci.append(f1)
+        var fib = 2
+        while fib < n + 2 {
+            let fibN = (arrayFibonacci[fib - 1]) + (arrayFibonacci[fib - 2])
+            arrayFibonacci.append(fibN)
+            fib += 1
+        }
+    }
+    return arrayFibonacci
+}
+
+print("Числа Фибоначчи", fibonacci(50))
+print("------------------------------", "\n")
+
+/*6. Заполнить массив из 100 элементов различными простыми числами. Натуральное число, большее единицы, называется простым, если оно делится только на себя и на единицу. Для нахождения всех простых чисел не больше заданного числа n, следуя методу Эратосфена, нужно выполнить следующие шаги:
+a. Выписать подряд все целые числа от двух до n (2, 3, 4, ..., n).
+b. Пусть переменная p изначально равна двум — первому простому числу.
+c. Зачеркнуть в списке числа от 2 + p до n, считая шагом p..
+d. Найти первое не зачёркнутое число в списке, большее, чем p, и присвоить значению переменной p это число.
+e. Повторять шаги c и d, пока возможно*/
  
